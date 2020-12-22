1134bbc68da5c8473701423ef0b1d17aed05cef4
Emre Kelleci <>
2020-12-22 13:17:44 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: "Place Experience",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-wordpress-experimental",
+      options: {
+        url: "https://mis-resources.com/graphql",
+      },
+    },
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
